ecc5c72224d6ae4f3f6e1a1dceeb323c824899b0
Imene Hassine <hassine.imen@eniso.u-sousse.tn>
2021-12-09 08:51:38 +0100

    Update

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -31,15 +31,6 @@ module.exports = (sequelize, Sequelize) => {
     password: {
       type: Sequelize.STRING
     },
-    // confirm_mail: {
-    //   type: Sequelize.BOOLEAN, allowNull: true, defaultValue: "0"
-    // }   ,  
-    // confirmMailToken: {
-    //   type: Sequelize.STRING
-    // },  
-    // tokenExpires: {
-    //   type: Sequelize.DATE
-    // },
     resetPasswordToken: {
       type: Sequelize.STRING
     },  
