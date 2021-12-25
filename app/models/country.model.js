3ce2449dea847a3d8b72b72fc7a88596db1ffed6
dev-mobile <hassine.imen@eniso.u-sousse.tn>
2021-12-25 22:02:06 +0100

    Clean code with  eslint

--- app/models/country.model.js
+++ app/models/country.model.js
@@ -1,29 +1,28 @@
 module.exports = (sequelize, Sequelize) => {
-    const Country = sequelize.define("Country", {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      title: {
-        type: Sequelize.STRING,
-        required: true,
-      },
-      createdBy: {
-        type: Sequelize.STRING,
-      },
-      updatedBy: {
-        type: Sequelize.STRING,
-      },
-      deletedBy: {
-        type: Sequelize.STRING,
-      },
-      deleted: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-      },
-    });
-    return Country;
-  };
-  
+  const Country = sequelize.define("Country", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      required: true,
+    },
+    createdBy: {
+      type: Sequelize.STRING,
+    },
+    updatedBy: {
+      type: Sequelize.STRING,
+    },
+    deletedBy: {
+      type: Sequelize.STRING,
+    },
+    deleted: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+  });
+  return Country;
+};
