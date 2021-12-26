ade5b1085af99045452129aff71dc0533668a97f
dev-mobile <hassine.imen@eniso.u-sousse.tn>
2021-12-26 20:19:47 +0100

    Clean with eslint

--- app/models/category.model.js
+++ app/models/category.model.js
@@ -1,32 +1,39 @@
 module.exports = (sequelize, Sequelize) => {
-  const Category = sequelize.define("Category", {
-    id: {
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  const Category = sequelize.define(
+    "Category",
+    {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        required: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        required: true,
+      },
+      createdBy: {
+        type: Sequelize.STRING,
+      },
+      updatedBy: {
+        type: Sequelize.STRING,
+      },
+      deletedBy: {
+        type: Sequelize.STRING,
+      },
+      deleted: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
     },
-    title: {
-      type: Sequelize.STRING,
-      required: true,
-    },
-    description: {
-      type: Sequelize.STRING,
-      required: true,
-    },
-    createdBy: {
-      type: Sequelize.STRING,
-    },
-    updatedBy: {
-      type: Sequelize.STRING,
-    },
-    deletedBy: {
-      type: Sequelize.STRING,
-    },
-    deleted: {
-      type: Sequelize.INTEGER,
-      defaultValue: 0,
-    },
-  });
+    {
+      sequelize,
+      modelName: "Category",
+    }
+  );
   return Category;
 };
