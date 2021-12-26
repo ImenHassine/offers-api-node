2182c74bdf8d3256694e02c29a18a3075d218ee3
dev-mobile <hassine.imen@eniso.u-sousse.tn>
2021-12-26 20:56:27 +0100

    Add category route

---
+++ app/routes/category.routes.js
@@ -0,0 +1,24 @@
+module.exports = (app) => {
+  const category = require("../controllers/category");
+  const validate = require("../middleware/validation");
+  const { schema, patchSchema } = require("../schema/category");
+  const {
+    createdSchema,
+    updatedSchema,
+    deletedSchema,
+  } = require("../schema/common");
+
+  const router = require("express").Router();
+
+  router.post("/", [validate(schema), validate(createdSchema)], category.add);
+  router.get("/", category.findAll);
+  router.get("/:id", category.findOne);
+  router.delete("/:id", validate(deletedSchema), category.delete);
+  router.patch(
+    "/:id",
+    [validate(patchSchema), validate(updatedSchema)],
+    category.patch
+  );
+
+  app.use("/api/category", router);
+};
